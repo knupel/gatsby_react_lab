8bb8e862dce35bb76145b367f0fd62c344efc0c0
Stan le Punk <stanislasiii@gmail.com>
2020-11-18 18:26:56 +0100

    an other step of the Gatsby way

--- src/pages/image_grid_specific_folder.js
+++ src/pages/image_grid_specific_folder.js
@@ -9,14 +9,16 @@ const img_grid_style = {
 }
 
 export default ({ data }) => {
-  return <div>
-    <Layout title="IMAGE GRID FROM SPECIFIC FOLDER"></Layout>
-    <div style={img_grid_style}>
-      {data.allFile.edges.map(({ node }) => (
-        <Img fluid={node.childImageSharp.fluid} />
-      ))}
+  return (
+    <div>
+      <Layout title="IMAGE GRID FROM SPECIFIC FOLDER"></Layout>
+      <div style={img_grid_style}>
+        {data.allFile.edges.map(({ node }) => (
+          <Img fluid={node.childImageSharp.fluid} />
+        ))}
+      </div>
     </div>
-  </div>
+  )
 }
 
 export const query = graphql`
@@ -36,132 +38,3 @@ export const query = graphql`
     }
   }
 `
-// extension: { regex: "/jpeg|jpg/" }
-
-// import React from "react"
-// import Img from "gatsby-image"
-// import { StaticQuery, graphql } from "gatsby"
-// function renderImage(file) {
-//   console.log(file)
-//   return <Img fluid={file.node.childImageSharp.fluid} />
-// }
-
-// const MyImg = function (props) {
-//   return (
-//     <StaticQuery
-//       query={graphql`
-//         query {
-//           images: allFile(
-//             filter: { extension: { regex: "/jpeg|jpg|png|gif/" } }
-//           ) {
-//             edges {
-//               node {
-//                 extension
-//                 relativePath
-//                 childImageSharp {
-//                   fluid(maxWidth: 1000) {
-//                     ...GatsbyImageSharpFluid
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `}
-//       render={({ images }) =>
-//         renderImage(
-//           images.edges.find(image => image.node.relativePath === props.src)
-//         )
-//       }
-//     />
-//   )
-// }
-
-/*
- {
- allFile(filter: { sourceInstanceName: { eq: "champImages" } }) {
- edges {
- node {
- extension
- relativePath
- childImageSharp {
- fluid {
- ...GatsbyImageSharpFluid
- }
- }
- }
- }
- }
- }
- */
-/*
- export const query = graphql`
- query {
- allImageSharp(filter: { id: { regex: "/media/" } }) {
- edges {
- node {
- id
- fluid(maxWidth: 200, maxHeight: 200) {
- ...GatsbyImageSharpFluid
- }
- }
- }
- }
- }
- `
- */
-
-// query HeaderQuery {
-//   exampleImage: file(
-//     extension: {eq: "jpg"},
-//     name: {eq: "exampleImage"}
-//   ) {
-//     childImageSharp {
-//       fluid {
-//         src
-//         srcSet
-//         sizes
-//         srcSetWebp
-//       }
-//     }
-//   }
-// }
-
-// allImageSharp(filter: { sourceInstanceName: { eq: "media" } }) {
-// allImageSharp {
-// allFile(filter: { sourceInstanceName: { eq: "media" } }) {
-
-// images: allImageSharp(filter: { id: { regex: "/media/" } }) {
-
-// all picture from folder data
-// export const query = graphql`
-//   query {
-//     allImageSharp {
-//       edges {
-//         node {
-//           id
-//           fluid(maxWidth: 200, maxHeight: 200) {
-//             ...GatsbyImageSharpFluid
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-
-// export const query = graphql`
-//   query {
-//     allFile(filter: { sourceInstanceName: { eq: "media" } }) {
-//       childImageSharp {
-//         edges {
-//           node {
-//             id
-//             fluid(maxWidth: 200, maxHeight: 200) {
-//               ...GatsbyImageSharpFluid
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
