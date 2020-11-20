1788bab897690b57326b39f8d640b5a3301780d1
Stan le Punk <stanislasiii@gmail.com>
2020-11-20 15:56:41 +0100

    oeil rouge

--- src/pages/react_info.js
+++ src/pages/react_info.js
@@ -1,18 +1,19 @@
 import React, { useState, useLayoutEffect, useRef } from "react"
-
-// to pass gatsby built
-function MyComponent() {
-  let w
-  let h
-
+function set_canvas(canvas) {
+  // need that to pass gatsby build
   if (typeof window !== `undefined`) {
-    w = window.innerWidth
-    h = window.innerHeight
+    canvas[0] = window.innerWidth
+    canvas[1] = window.innerHeight
   }
+}
 
-  const [windowSize, _setWindowSize] = useState([w, h])
+function WindowInfo() {
+  let canvas = [0, 0]
+  set_canvas(canvas)
+
+  const [windowSize, _setWindowSize] = useState(canvas)
+
   const windowSizeRef = useRef(windowSize)
-
   const setWindowSize = value => {
     windowSizeRef.current = value
     _setWindowSize(value)
@@ -24,12 +25,9 @@ function MyComponent() {
     function onWindowResize(event) {
       // You can access the state value like this
       let windowSizeValue = windowSizeRef.current
-      if (typeof window !== `undefined`) {
-        w = window.innerWidth
-        h = window.innerHeight
-      }
+      set_canvas(canvas)
       // And set the state value like this
-      setWindowSize([h, w])
+      setWindowSize([canvas[0], canvas[1]])
     }
 
     window.addEventListener("resize", onWindowResize)
@@ -47,43 +45,7 @@ function MyComponent() {
   )
 }
 
-// gatsby develope
-// function MyComponent() {
-//   const [windowSize, _setWindowSize] = useState([
-//     window.innerWidth,
-//     window.innerHeight,
-//   ])
-//   const windowSizeRef = useRef(windowSize)
-
-//   const setWindowSize = value => {
-//     windowSizeRef.current = value
-//     _setWindowSize(value)
-//   }
-
-//   // Here we can normally use setWindowSize([x,y]) and windowSize
-
-//   useLayoutEffect(() => {
-//     function onWindowResize(event) {
-//       // You can access the state value like this
-//       let windowSizeValue = windowSizeRef.current
-
-//       // And set the state value like this
-//       setWindowSize([window.innerWidth, window.innerHeight])
-//     }
-
-//     window.addEventListener("resize", onWindowResize)
-
-//     return () => {
-//       window.removeEventListener("resize", onWindowResize)
-//     }
-//   }, [])
-//   // }
-
-//   return (
-//     <div>
-//       Hi There, screen is {windowSize[0]} x {windowSize[1]}
-//     </div>
-//   )
-// }
-
-export default MyComponent
+function ReactInfo() {
+  return WindowInfo()
+}
+export default ReactInfo
