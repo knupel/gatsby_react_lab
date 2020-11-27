9963c6754d29e4171bf8097a602445c3810fa3cc
Stan le Punk <stanislasiii@gmail.com>
2020-11-27 11:16:46 +0100

    Update link_location_state.js

--- src/pages/link_location_state.js
+++ src/pages/link_location_state.js
@@ -2,10 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 
 export default ({ location }) => {
-  return (
-    <div>
-      <Layout title="FROM THE HEAD QUARTER"></Layout>
-      <div>{location.state.info}</div>
-    </div>
-  )
+  // console.log("ICI PARIS")
+  // console.log("location.state.info", location.state.info)
+  if (!location.state) {
+    return null
+  } else {
+    return (
+      <div>
+        <Layout title="FROM THE HEAD QUARTER"></Layout>
+        <div>{location.state.info}</div>
+      </div>
+    )
+  }
 }
