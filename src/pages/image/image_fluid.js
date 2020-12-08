1a247a980064e9694040be536b64b036a9407926
Stan le Punk <stanislasiii@gmail.com>
2020-12-08 18:59:21 +0100

    test image

--- src/pages/image/image_fluid.js
+++ src/pages/image/image_fluid.js
@@ -3,20 +3,20 @@ import Img from "gatsby-image"
 import Layout from "../../components/layout"
 import { graphql } from "gatsby"
 
+/**
+ * For the moment and for an unknow reason the image is call from the roor folder media.
+ * like set in the gatsby-config resolve: `gatsby-source-filesystem`,
+ */
 export const query = graphql`
   query {
-    mobileImage: file(
-      relativePath: { eq: "Boxon_1_0_2015_11_450x300_72_dpi_Stan_le_Punk.jpg" }
-    ) {
+    mobileImage: file(relativePath: { eq: "2020_8_11_contagion_6_3.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1000, quality: 100) {
           ...GatsbyImageSharpFluid
         }
       }
     }
-    desktopImage: file(
-      relativePath: { eq: "Boxon_1_0_2015_11_450x300_72_dpi_Stan_le_Punk.jpg" }
-    ) {
+    desktopImage: file(relativePath: { eq: "2020_8_11_contagion_6_3.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 2000, quality: 100) {
           ...GatsbyImageSharpFluid
@@ -27,6 +27,14 @@ export const query = graphql`
 `
 
 export default function Image({ data }) {
+  if (data.mobileImage === null) {
+    return (
+      <div>
+        <Layout title="IMAGE FLUID"></Layout>
+        <p>No image match with your path, check your media</p>
+      </div>
+    )
+  }
   const sources = [
     data.mobileImage.childImageSharp.fluid,
     {
