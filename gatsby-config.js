56e7c8070d5401cae1ea147b04b6ce3398733393
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2022-12-10 12:01:59 +0100

    big mess

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,87 +1,43 @@
-// module.exports = {
-//   siteMetadata: {
-//     siteUrl: "https://www.yourdomain.tld",
-//     title: "test",
-//   },
-//   plugins: [],
-// };
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
 
-// https://medium.com/codait/environment-variables-or-keeping-your-secrets-secret-in-a-node-js-app-99019dfff716
-require("dotenv").config();
-
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
   siteMetadata: {
-    title: `Knupel Labo Gatsby React `,
-    description: `Here you find my work around the gatsby and React concept to understand the jungle web`,
-    author: `Knupel`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    // SCSS
-    `gatsby-plugin-sass`,
-    // FONT
+    `gatsby-plugin-image`,
     {
-      resolve: "gatsby-plugin-web-font-loader",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        typekit: {
-          id: process.env.TYPEKIT_ID,
-        },
-      },
-    },
-    // UI / UX
-    // `gatsby-plugin-material-ui`,
-
-
-    // IMAGE
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        // Available options and their defaults:
-        base64Width: 20,
-        // forceBase64Format: ``, // valid formats: png,jpg,webp // don't work on OSX
-        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
-        stripMetadata: true,
-        defaultQuality: 50,
-        failOnError: true,
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-image`,
-
-    // FILE SYSTEME
+    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `media_art`,
-        path: `${__dirname}/media/media_art`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `tdm`,
-        path: `${__dirname}/media/photo/tdm`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
-    // call all media for an unknow reason
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `media`,
-        path: `${__dirname}/media`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
-    // CONTENTFUL
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        enableTags: true,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
   ],
-};
+}
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,18 @@
-// You need this part to build and deploy your app for the Processing part
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-  if (stage === "build-html" || stage === "develop-html") {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /p5/,
-            // use: loaders.null(),
-            use: "null-loader",
-          },
-        ],
-      },
-    });
-  }
-};
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyNode['createPages']}
+ */
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions
+  createPage({
+    path: "/using-dsg",
+    component: require.resolve("./src/templates/using-dsg.js"),
+    context: {},
+    defer: true,
+  })
+}
