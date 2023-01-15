e39aaaccd5e01c213901589f7269c67da3258a4d
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-01-15 10:10:40 +0100

    fix the bug of embed link code sandbox

--- src/pages/misc/code_sandbox.js
+++ src/pages/misc/code_sandbox.js
@@ -4,9 +4,15 @@ import Layout from "../../components/struct/layout"
 // https://www.gatsbyjs.com/plugins/gatsby-remark-embedded-codesandbox/
 // https://codesandbox.io/docs/learn/getting-started/your-first-sandbox#define-api
 // https://github.com/uiwjs/react-codesandbox
+// https://codesandbox.io/docs/learn/sandboxes/embedding
 
+
+// src="https://codesandbox.io/embed/boucle-2-f81ukk?fontsize=14&hidenavigation=1&theme=dark"
 
 const CodeSandbox = () => {
+	const box = {
+		margin: "20px",
+	}
 	const style = {
 		width: "100%",
 		height: "500px",
@@ -17,10 +23,10 @@ const CodeSandbox = () => {
 
 	return (
 		<div>
-			<Layout title="Code Sandbox embed"></Layout>
-			<div>
+			<Layout title="Embed Code Sandbox"></Layout>
+			<div style={box}>
 				<iframe 
-					src="https://codesandbox.io/embed/boucle-2-f81ukk?fontsize=14&hidenavigation=1&theme=dark"
+					src="https://codesandbox.io/embed/boucle-2-f81ukk?codemirror=1"
 					style={style}
 					title="boucle 2"
 					allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
@@ -34,12 +40,3 @@ const CodeSandbox = () => {
 
 export default CodeSandbox;
 
-
-
-
-{/* <iframe src="https://codesandbox.io/embed/boucle-2-f81ukk?fontsize=14&hidenavigation=1&theme=dark"
-     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-     title="boucle 2"
-     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-   ></iframe> */}
