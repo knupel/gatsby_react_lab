fb58ea6f0076a38c157b518b2adfc10164180857
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-05-11 23:03:46 +0200

    try and fail on github api

--- src/pages/api/api_github_token.tsx
+++ src/pages/api/api_github_token.tsx
@@ -4,20 +4,42 @@ import { useState } from "react";
 // APP
 import Layout from "../../components/struct/layout"
 
-// https://github.com/settings/tokens/new
-// https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents
 // https://levelup.gitconnected.com/how-to-upload-images-to-github-via-javascript-59163b8fff27
+
+
+// https://github.com/settings/tokens/new
+
+// https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents
+
 // https://medium.com/axlewebtech/upload-a-file-in-github-using-github-apis-dbb6f38cc63
 // https://stackoverflow.com/questions/62729099/react-get-the-input-type-file-value
+
+// FAKE PATH
 // https://stackoverflow.com/questions/41453224/uploading-a-file-with-reactjs-and-dealing-with-c-fakepath-file
+// FORM DATA
 // https://developer.mozilla.org/en-US/docs/Web/API/FormData
+
+// 422 Unprocessable Entity pour les fois ou ça charge les données
+// https://stackoverflow.com/questions/51255506/postman-error-422-unprocessable-entity-with-github-api-what-is-it-and-how-do
+
+// OCTOKIT
+// https://docs.github.com/en/rest/guides/scripting-with-the-rest-api-and-javascript?apiVersion=2022-11-28
+
 export default function ApiGithubToken() {
-  const [name, setName] = useState('');
-  const [selectedFile, setSelectedFile] = useState(null);
+  // const [name, setName] = useState('');
+  const [file, set_file] = useState(null);
+
+  // function set_file() {
+
+  // }
+
+  // console.log("process.env.GATSBY_TOKEN_GITHUB", process.env.GATSBY_TOKEN_GITHUB);
+  // console.log("process.env.TYPEKIT_ID", process.env.TYPEKIT_ID);
+  // console.log("process.env.GATSBY_GOOGLE_MAP_ID",process.env.GATSBY_GOOGLE_MAP_ID);
   const load = e => {
     
-    console.log("event.target.value", e.target.value);
-    console.log("event.target.files", e.target.files);
+    // console.log("event.target.value", e.target.value);
+    // console.log("event.target.files", e.target.files);
 
     if(e.target !== null) {
       let files = e.target.files;
@@ -25,28 +47,41 @@ export default function ApiGithubToken() {
         let reader = new FileReader();
         reader.readAsDataURL(files[0]);
         reader.onload = (e) => {
-          console.log('data: ', e.target.result);
-        };
-        setSelectedFile(e.target.value);
-        // setSelectedFile(e.target.value);
-        console.log("selectedFile", selectedFile);
+          set_file(files[0]);
+        }
+        // reader.onload = (e) => {
+        //   // console.log('data: ', e.target.result);
+        //   // set_file(e.target.result);
+        //   // const selectedFile = document.getElementById("input").files[0];
+        //  if(e.target !== null) {
+        //   // set_file(files[0]);
+        //   console.log("files[0]", files[0]);
+        //  }
+          
+        // };
+        // console.log("file", file);
+        // console.log("e.target", e.target);
+        // console.log("e.target.files[0]", e.target.files[0]);
+        // console.log("e.target.result", e.target.result);
+        // console.log("e.target.value", e.target.value);
+        if(files[0] !== null) {
+          //console.log("selectedFile", file);
+          const git= {
+            owner : 'knupel',
+            repo: 'gatsby_react_lab',
+            path: `media/test/`,
+            token: process.env.GATSBY_TOKEN_GITHUB,
+          }
+          // console.log("git", git);
+          console.log("files[0]", files[0]);
+          console.log("reader", reader);
+          upload_file(git, reader);
+          // upload_file(git, files[0]);
+          // upload_file(git, file);
+        }
+      }
     }
-   
-
-    }
-
-
-    // cause crash
-    // const selectedFile = event.target.input.files[0];
-    //     console.log('Selected File', selectedFile);
-    //     var data = new FormData();
-    //     data.append('file', selectedFile);
-    //     console.log('Form Data', data);
-
-    // cause a crash
-    // if (event.target.input.files.length) {
-    //   console.log("event", event.target.input.files[0]);
-    // }
+    // console.log("after file", file)
   }
   return (
     <div>
@@ -55,15 +90,14 @@ export default function ApiGithubToken() {
         link="true"
       >
       </Layout>
-      <form>
+      {/* <form> */}
       {/* <form onSubmit={load}> */}
-        <p>Sélectionner le fichier à déposer sur Github</p>
-        <p>Select your file to upload in Github</p>
+        <p>Sélectionner le fichier à déposer sur Github / Select your file to upload in Github</p>
         <input type="file" name="input" onChange={load}/>
         <br/>
         <br/>
         <button type="submit">CHARGEZ</button>
-      </form>
+      {/* </form> */}
       {/* <div>
         <p>Sélectionner le fichier à déposer sur Github</p>
         <p>Select your file to upload in Github</p>
@@ -77,22 +111,39 @@ export default function ApiGithubToken() {
 }
 
 // HTTP ACCES
-function uploadImage(data) {
-  return fetch(
-    `https://api.github.com/repos/${data.owner}/${data.repo}/contents/${data.name}`,
+// interface Props {
+//   data : any;
+//   git: any;
+
+// }
+const upload_file = async (git: { owner: string; repo: string; path: string; token: string | undefined; }, data: any) => {
+  let final_path = `https://api.github.com/repos/${git.owner}/${git.repo}/contents/${git.path}`;
+  // console.log("final_path", final_path);
+  // console.log("token", git.token);
+  console.log("data", data);
+  const res = await fetch(final_path,
     {
       method: "PUT",
       headers: {
         Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${data.token}`
+        Authorization: `Bearer ${git.token}`
       },
       body: JSON.stringify({
         message: "Téléversement de votre fichier",
-        content: data.content
+        // content: data,
+        content: data.split('base64,')[1]
+        // content: data.content
       })
     }
-  ).then((res) => res.json());
+  );
+  return await res.json();
 }
+
+
+
+
+
+
 // Octokit.js
 // https://github.com/octokit/core.js#readme
 /*
