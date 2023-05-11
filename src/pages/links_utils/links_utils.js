056c7dff9fa7b4d4dde38d277c5d581878d568e2
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-05-11 12:46:00 +0200

    Update links_utils.js

--- src/pages/links_utils/links_utils.js
+++ src/pages/links_utils/links_utils.js
@@ -44,6 +44,19 @@ function SectionCode() {
       />
       <NewTabLink href="https://putaindecode.io/" title="Blog de code, sur le putain de code"/>
       <NewTabLink href="https://www.i18next.com/" title="Module et plugin pour traduire des sites... à explorer ou pas"/>
+      <NewTabLink href="https://github.com/Kristories/awesome-guidelines"
+                  title="coding guide by language and companies"
+      />
+      <NewTabLink href="https://lincnil.github.io/Guide-RGPD-du-developpeur/"
+                  title="RGPD, guide pour les développeurs"
+      />
+      <NewTabLink href="https://www.cnil.fr/fr/outil-pia-telechargez-et-installez-le-logiciel-de-la-cnil"
+                  title="AIPD, PIA logiciel développé par la CNIL pour tester le RGPD / AIPD"
+      />
+      <NewTabLink href="https://github.com/markdown-templates/markdown-emojis"
+                  title="Emoji to add in your github markdown"
+      />
+      
     </div>
   );
 }
